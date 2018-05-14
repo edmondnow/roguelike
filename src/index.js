affe046521e36bd2b70c27bf2cc39c9ea781adea
edmondnow <edmond.bitay@gmail.com>
2018-05-14 21:04:24 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app';
+
+
+
+
+ReactDOM.render(
+  <div>
+     <App />
+  </div>
+
+  , document.querySelector('.container'));
